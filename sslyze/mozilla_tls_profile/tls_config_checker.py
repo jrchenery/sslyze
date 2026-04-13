f3d3fbc352c129f05de4b694209a2188d94ac422
Julie Chenery <julie.chenery@bentley.com>
2026-04-13 14:24:16 +0100

    'old' has been removed from the Mozilla TLS config

--- sslyze/mozilla_tls_profile/tls_config_checker.py
+++ sslyze/mozilla_tls_profile/tls_config_checker.py
@@ -83,7 +83,7 @@ class _MozillaTlsProfileAsJson(pydantic.BaseModel):
 class TlsConfigurationEnum(str, Enum):
     MOZILLA_MODERN = "modern"
     MOZILLA_INTERMEDIATE = "intermediate"
-    MOZILLA_OLD = "old"
+    #MOZILLA_OLD = "old"
     CUSTOM = "custom"
 
 
